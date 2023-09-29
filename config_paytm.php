6af2a77046e3ecb7d429ca57bf7e56387305c6b3
TUSHAR KANTI PATRA <tkantipatra357@gmail.com>
2023-09-30 00:20:05 +0530

    change

---
+++ config_paytm.php
@@ -0,0 +1,13 @@
+
+define('PAYTM_ENVIRONMENT', 'TEST'); // change value PROD for production
+define('PAYTM_MERCHANT_KEY', 'vsbi51****uKL0#3 ');
+define('PAYTM_MERCHANT_MID', 'codefi*****858443');
+define('PAYTM_MERCHANT_WEBSITE', 'WEBS***ING');
+1
+2
+3
+4
+define('PAYTM_ENVIRONMENT', 'TEST'); // change value PROD for production
+define('PAYTM_MERCHANT_KEY', 'vsbi51****uKL0#3 ');
+define('PAYTM_MERCHANT_MID', 'codefi*****858443');
+define('PAYTM_MERCHANT_WEBSITE', 'WEBS***ING');
---
+++ include/footer.php
@@ -0,0 +1,38 @@
+      <footer class="footer">
+          <div class="container">
+
+
+              <div class="bottom-footer">
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                          <h5>Another Online Payment Options</h5>
+                          <ul>
+                              <li>
+                                  <a href="paytm.jpg"> <img src="images/paytm3.png" alt="Paytm"> </a>
+                              </li>
+                              <li>
+                                  <a href="GooglePay.png"> <img src="images/gpay.png"width="50" height="50" alt="Google pay"> </a>
+                              </li>
+                              <li>
+                                  <a href="PhonePey.png"> <img src="images/php.png" width="50" height="50" alt="Phonepay"> </a>
+                              </li>
+                              
+                          </ul>
+                      </div>
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                          <h5>Address</h5>
+                          <p>Medinipur, Kolkata, West bengal.</p>
+                          <h5>Phone: +91 818079079</a></h5>
+                      </div>
+                      <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                          <h5>Addition informations</h5>
+                          <p>Join thousands of other restaurants who benefit from having partnered with us.</p>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </footer>
+
+
+  
